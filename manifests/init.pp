e67d0c1b64a3927b8ae51315c37634b78fd7cddc
Mustafa Nural <mvnural@gmail.com>
2025-07-30 14:13:24 -0400

    Create default certificate in puppet.
    httpd default cert creation workflow does not play well with our puppet apache setup as it expects the default /etc/httpd/conf.d/ssl.conf config, otherwise it skips it.
    
    So we inline the relevant bits of the script here to let puppet create it on install.

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,6 +18,28 @@ class ebrc_httpd_setup (
     fail('OS not supported. Expect a RedHat family.')
   }
 
+  $default_cert_command = @(END)
+  #!/usr/bin/bash
+  set -e
+
+  FQDN=`hostname`
+
+  sscg -q                                                             \
+    --cert-file           /etc/pki/tls/certs/localhost.crt         \
+    --cert-key-file       /etc/pki/tls/private/localhost.key       \
+    --ca-file             /etc/pki/tls/certs/localhost.crt         \
+    --dhparams-file       /tmp/dhparams.pem                        \
+    --lifetime            365                                      \
+    --hostname            $FQDN                                    \
+    --email               root@$FQDN
+  | END
+
+  exec { "create-default-httpd-certs":
+    command => $default_cert_command,
+    creates => "/etc/pki/tls/certs/localhost.crt",
+    require => Class['::apache::mod::ssl'],
+  }
+
   contain '::ebrc_httpd_setup::server'
   contain '::ebrc_httpd_setup::config'
 
